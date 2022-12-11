0e1d40c1013bbaa1c4a0a444ec4a27f6ddba7bfc
Sicco <sdotter@gmail.com>
2022-12-11 19:51:54 +0100

    Added .net 7.0 core project (for running on raspberry pi)

---
+++ Core/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TC421
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Main main = new Main();
+            Thread t = new Thread(main.DoWork);
+
+            t.IsBackground = true;
+            t.Start();
+
+            while (true)
+            {
+                var keyInfo = Console.ReadKey();
+                if (keyInfo.Key == ConsoleKey.C && keyInfo.Modifiers == ConsoleModifiers.Control)
+                {
+                    main.KeepGoing = false;
+                    break;
+                }
+            }
+            t.Join();
+        }
+    }
+}
